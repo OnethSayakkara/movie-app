22f0fc545c05af6e392929a48e53de6ad41c9b1e
oneth sayakkara <onethsayakkara@gmail.com>
2025-06-23 22:56:17 +0530

    setup tailwind css to app

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1" >
+      <Text className="text-4xl text-red-600">welcome boss</Text>
+    </View>
+  );
+}
